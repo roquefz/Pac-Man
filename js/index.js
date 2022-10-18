6753a511c0073e530508f97f2910d44ddd559a72
Jose Zuccon <joseff.zuccon@gmail.com>
2022-10-18 21:30:30 +0100

    prepares for merge

--- js/index.js
+++ js/index.js
@@ -2,68 +2,78 @@ const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 const getScore = document.getElementById("score");
 const getHp = document.getElementById("hp");
+const startBtn = document.getElementById("start-btn");
+
+// Load game
+let isRunning;
+window.onload = () => {
+  startBtn.addEventListener("click", () => {
+    console.log("hi");
+    if (!isRunning) {
+      updateGameArea();
+    }
+  });
+};
 
 // Player and ghost
 
-const player = new Pacman( {
+const player = new Pacman({
   position: {
-    x: (25 + (25 / 2)),
-    y: (25 + (25 / 2))
+    x: 25 + 25 / 2,
+    y: 25 + 25 / 2,
   },
   speed: {
     x: 0,
-    y: 0
-  }
- });
+    y: 0,
+  },
+});
 
-
-const ghosts = [ 
+const ghosts = [
   new Ghosts({
     position: {
-      x: (25 + (25 / 2)),
-      y: (25 * 18 + (25 / 2))
+      x: 25 + 25 / 2,
+      y: 25 * 18 + 25 / 2,
     },
-    speed:  {
+    speed: {
       x: 0,
-      y: -1
+      y: -1,
     },
-     color: "#FF0000",
+    color: "#FF0000",
   }),
   new Ghosts({
     position: {
-      x: (25 * 18 + (25 / 2)),
-      y: (25 + (25 / 2))
+      x: 25 * 18 + 25 / 2,
+      y: 25 + 25 / 2,
     },
-    speed:  {
+    speed: {
       x: 0,
-      y: 1
+      y: 1,
     },
-     color: "#FFB8FF",
+    color: "#FFB8FF",
   }),
   new Ghosts({
     position: {
-      x: (25 * 10 + (25 / 2)),
-      y: (25 * 3 + (25 / 2))
+      x: 25 * 10 + 25 / 2,
+      y: 25 * 3 + 25 / 2,
     },
-    speed:  {
+    speed: {
       x: 1,
-      y: 0
+      y: 0,
     },
-     color: "#00FFFF",
+    color: "#00FFFF",
   }),
   new Ghosts({
     position: {
-      x: (25 * 10 + (25 / 2)),
-      y: (25 * 15+ (25 / 2))
+      x: 25 * 10 + 25 / 2,
+      y: 25 * 15 + 25 / 2,
     },
-    speed:  {
+    speed: {
       x: -1,
-      y: 0
+      y: 0,
     },
-     color: "#FFB852",
+    color: "#FFB852",
   }),
 ];
-
 
 // HP and Score
 
@@ -74,97 +84,115 @@ let hp = 3;
 
 const keys = {
   ArrowUp: {
-    pressed: false
+    pressed: false,
   },
   ArrowDown: {
-    pressed: false
+    pressed: false,
   },
   ArrowLeft: {
-    pressed: false
+    pressed: false,
   },
   ArrowRight: {
-    pressed: false
-  }
-}
-let prevKey = '';
+    pressed: false,
+  },
+};
+let prevKey = "";
 
 function updateGameArea() {
+  isRunning = true;
   context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
   player.newPosition();
-  playerMovement() 
+  playerMovement();
   const requestID = requestAnimationFrame(updateGameArea);
 
-  ghosts.forEach(ghost => {
+  ghosts.forEach((ghost) => {
     ghost.draw();
     ghost.newPosition();
 
     if (collisionWith(ghost, player)) {
       death.play();
-      cancelAnimationFrame(requestID)
-      hp = hp - 1;
-      getHp.innerHTML = `HP: ${hp}`;
+      if (hp > 1) {
+        player.position.x = 25 * 10 + 25 / 2;
+        player.position.y = 25 * 9 + 25 / 2;
+        player.speed.x = 0;
+        player.speed.y = 0;
+        hp--;
+        console.log(hp);
+        getHp.innerHTML = `HP: ${hp}`;
+      } else {
+        getHp.innerHTML = `HP: 0`;
+        isRunning = false;
+        cancelAnimationFrame(requestID);
+      }
     }
 
-    barriers.forEach((barrier) => { 
-      barrier.draw()
-      if ((ghost.speed.y === 1 || ghost.speed.y === -1) && detectCollision({
-        barrier: barrier,
-        player: ghost,
-      })) {
+    barriers.forEach((barrier) => {
+      barrier.draw();
+      if (
+        (ghost.speed.y === 1 || ghost.speed.y === -1) &&
+        detectCollision({
+          barrier: barrier,
+          player: ghost,
+        })
+      ) {
         ghost.speed.x = 0;
-        ghost.speed.y *= -1
+        ghost.speed.y *= -1;
       }
-      if ((ghost.speed.x === 1 || ghost.speed.x === -1) && detectCollision({
-        barrier: barrier,
-        player: ghost,
-      })) {
+      if (
+        (ghost.speed.x === 1 || ghost.speed.x === -1) &&
+        detectCollision({
+          barrier: barrier,
+          player: ghost,
+        })
+      ) {
         ghost.speed.x *= -1;
-        ghost.speed.y = 0
+        ghost.speed.y = 0;
       }
-    })
-  })
+    });
+  });
 
   // Map boundaries and collisions
-  barriers.forEach((barrier) => { 
-    barrier.draw()
-    if(detectCollision({
-      barrier: barrier,
-      player: player,
-    })) {
+  barriers.forEach((barrier) => {
+    barrier.draw();
+    if (
+      detectCollision({
+        barrier: barrier,
+        player: player,
+      })
+    ) {
       player.speed.x = 0;
-      player.speed.y = 0
+      player.speed.y = 0;
     }
-  })
-  scores.forEach((whiteDot, counter) => {   
-    whiteDot.draw()  
+  });
+  scores.forEach((whiteDot, counter) => {
+    whiteDot.draw();
     if (collisionWith(player, whiteDot)) {
-      scoreCounter++;
+      scoreCounter += 10;
       scores.splice(counter, 1);
-      if(scores.length === 0) {
-        getScore.innerHTML = 'You win! Game Over!';
+      if (scores.length === 0) {
+        isRunning = false;
+        getScore.innerHTML = "You win! Game Over!";
         cancelAnimationFrame(requestID);
       } else {
         eating.play();
-        getScore.innerHTML = `Score: ${scoreCounter - 1}`;
+        getScore.innerHTML = `Score: ${scoreCounter}`;
       }
     }
-  })
+  });
 }
-
-updateGameArea();
 
 // Animate player movement
 function playerMovement() {
-  if (keys.ArrowUp.pressed && prevKey === 'ArrowUp') {
+  if (keys.ArrowUp.pressed && prevKey === "ArrowUp") {
     player.rotation = 1.5 * Math.PI;
     player.speed.y = -1;
-  } else if (keys.ArrowDown.pressed && prevKey === 'ArrowDown') {
+  } else if (keys.ArrowDown.pressed && prevKey === "ArrowDown") {
     player.rotation = Math.PI / 2;
     player.speed.y = 1;
-  } else if (keys.ArrowLeft.pressed && prevKey === 'ArrowLeft') {
+  } else if (keys.ArrowLeft.pressed && prevKey === "ArrowLeft") {
     player.rotation = Math.PI;
     player.speed.x = -1;
-  } else if (keys.ArrowRight.pressed && prevKey === 'ArrowRight') {
+  } else if (keys.ArrowRight.pressed && prevKey === "ArrowRight") {
     player.rotation = 0;
     player.speed.x = 1;
   }
@@ -173,7 +201,10 @@ function playerMovement() {
 // Check if two circles are colliding
 function collisionWith(firstCircle, secondCircle) {
   if (
-    Math.hypot(secondCircle.position.x - firstCircle.position.x, secondCircle.position.y - firstCircle.position.y) <
+    Math.hypot(
+      secondCircle.position.x - firstCircle.position.x,
+      secondCircle.position.y - firstCircle.position.y
+    ) <
     secondCircle.radius + firstCircle.radius
   ) {
     return true;
@@ -181,15 +212,14 @@ function collisionWith(firstCircle, secondCircle) {
 }
 
 // Checks if a circle and a rectangle are colliding
-function detectCollision({
-  barrier,
-  player
-}) {
+function detectCollision({ barrier, player }) {
   return (
-    player.position.y - player.radius + player.speed.y <= barrier.y + barrier.height &&
+    player.position.y - player.radius + player.speed.y <=
+      barrier.y + barrier.height &&
     player.position.x + player.radius + player.speed.x >= barrier.x &&
     player.position.y + player.radius + player.speed.y >= barrier.y &&
-    player.position.x - player.radius + player.speed.x <= barrier.x + barrier.width
+    player.position.x - player.radius + player.speed.x <=
+      barrier.x + barrier.width
   );
 }
 
@@ -199,23 +229,22 @@ document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "ArrowUp":
       keys.ArrowUp.pressed = true;
-      prevKey = 'ArrowUp';
+      prevKey = "ArrowUp";
       break;
     case "ArrowDown":
       keys.ArrowDown.pressed = true;
-      prevKey = 'ArrowDown';
+      prevKey = "ArrowDown";
       break;
     case "ArrowLeft":
       keys.ArrowLeft.pressed = true;
-      prevKey = 'ArrowLeft';
+      prevKey = "ArrowLeft";
       break;
     case "ArrowRight":
       keys.ArrowRight.pressed = true;
-      prevKey = 'ArrowRight';
+      prevKey = "ArrowRight";
       break;
   }
 });
-
 
 document.addEventListener("keyup", (event) => {
   switch (event.key) {
